d301f4b976389a8f663602a39480771e91727ab5
Rafael Martins <rafaelmartins025@outlook.com>
2023-05-11 10:14:58 -0300

    Update rename-files.sh

--- rename-files.sh
+++ rename-files.sh
@@ -4,3 +4,10 @@ rename -vf 's/\(/_/' *
 rename -vf 's/\)/_/' *
 rename -vf 's/\ /_/' *
 rename -vf 's/ê/e/' *
+
+find . -type f -maxdepth 1 | xargs rename -vf 's/-/_/'
+find . -type f -maxdepth 1 | xargs rename -vf 's/:/_/'
+find . -type f -maxdepth 1 | xargs rename -vf 's/ /_/'
+find . -type f -maxdepth 1 | xargs rename -vf 'y/A-Z/a-z/'
+find . -type f -maxdepth 1 | xargs rename -vf 's/C/c/'
+find . -type f -maxdepth 1 | xargs rename -vf 's/A/a/'
